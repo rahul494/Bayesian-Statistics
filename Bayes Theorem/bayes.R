63e977f56cc4056acf169d81ebe9215184104d0f
Rahul Sharma <sharmarah494@gmail.com>
2023-08-07 09:51:04 -0400

    Update bayes.R

--- Bayes Theorem/bayes.R
+++ Bayes Theorem/bayes.R
@@ -1,1 +1,13 @@
-# WIP
+# Defining the parameters
+P_D <- 0.01  # Prevalence of the disease
+P_T_given_D <- 0.95  # Sensitivity
+P_notT_given_notD <- 0.95  # Specificity
+
+# Calculate the complement probabilities
+P_notD <- 1 - P_D
+P_T_given_notD <- 1 - P_notT_given_notD
+
+# Apply Bayes theorem
+P_D_given_T <- (P_T_given_D * P_D) / (P_T_given_D * P_D + P_T_given_notD * P_notD)
+
+print(P_D_given_T)

bc58b9af3fee691961cc0ebda2f7e9f08f2eab0e
Rahul Sharma <sharmarah494@gmail.com>
2023-08-07 09:57:19 -0400

    Update bayes.R

--- Bayes Theorem/bayes.R
+++ Bayes Theorem/bayes.R
@@ -1,4 +1,9 @@
-# Defining the parameters
+# Suppose we have a disease spreading into our population at a rate of 1%
+# and the probability of a true positive is 95% (someone is tested positive and actually has the disease)
+# and the probability of a true negative is also 95% (someone is tested negative and actually doesn't have the disease) 
+
+# What is the probability of a person having the disease given a positive test result?
+
 P_D <- 0.01  # Prevalence of the disease
 P_T_given_D <- 0.95  # Sensitivity
 P_notT_given_notD <- 0.95  # Specificity
@@ -7,7 +12,6 @@ P_notT_given_notD <- 0.95  # Specificity
 P_notD <- 1 - P_D
 P_T_given_notD <- 1 - P_notT_given_notD
 
-# Apply Bayes theorem
+# Formula for Bayes theorem
 P_D_given_T <- (P_T_given_D * P_D) / (P_T_given_D * P_D + P_T_given_notD * P_notD)
-
-print(P_D_given_T)
+P_D_given_T
